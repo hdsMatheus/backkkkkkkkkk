f31158715f1fcfb5a109bb92616c65b80543322e
matheus <no9946191@gmail.com>
2024-01-15 16:03:59 -0300

    oi

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 const segredoJWT = "seuSegredoSuperSecreto";
 
 app.use(cors());
